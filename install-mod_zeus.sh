710f3bc92bf58b3be6cc305aa7c20d4dab85db7f
Benjamin H. Graham <bhgraham1@gmail.com>
2012-08-15 23:07:45 -0500

    added install script

---
+++ install-mod_zeus.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Author - Benjamin H. Graham
+# Email - bhgraham1@gmail.com
+# Copyright - 2012 Administr8 (http://administr8.me/)
+
+DESC="Apache mod_zeus installer.";
+SCRIPT="modzeus";
+
+# Install: bash <(GET a8.lc/modzeus) --install
+# Usage: bash <(GET a8.lc/modzeus) -q
+
+VERSION="1.0.0 - 08/15/2012";
+
+# Changelog:
+# 1.0.0 - Created modzeus for debian and redhat
+
+# TODO:
+# Add apache1 support as well
+
+# source Administr8 common functions
+GET=GET; hash GET 2>/dev/null || { GET="curl -s"; }
+x=$(mktemp); $GET a8.lc/a8rc > $x; source $x;
+
+if [ -f /etc/redhat-release ]; then
+    a8distro="redhat";
+fi;
+if [ -f /etc/debian_version ]; then
+    a8distro="debian";
+fi;
+
+if [[ ${!#} != "-q" ]]; then
+	a8info "Downloading current mod_zeus.c from Github";
+fi;
+wget -q --no-check-certificate https://github.com/bhgraham/mod_zeus/raw/master/apache-2.x/mod_zeus.c
+
+a8info "Installing for apache2 on $a8distro";
+
+if [[ $a8distro == "redhat" ]]; then
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Downloading the httpd-devel package with yum."
+	fi;
+	yum -q -y install httpd-devel.x86_64 gcc
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Compiling mod_zeus.c..."
+	fi;
+	apxs -c -i mod_zeus.c
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Adding module configuration to Apache2."
+	fi;
+	echo "LoadModule zeus_module modules/mod_zeus.so" > /etc/httpd/conf.d/zeus.conf
+	echo "ZeusEnable On" >> /etc/httpd/conf.d/zeus.conf
+	echo "ZeusLoadBalancerIP *" >> /etc/httpd/conf.d/zeus.conf
+#	echo "ZeusLoadBalancerIP 10.100.3.23 10.100.3.24" >> /etc/httpd/conf.d/zeus.conf
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Restarting Apache (httpd)..."
+	fi;
+	service httpd graceful;
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Done."
+	fi;
+
+elif [[ $a8distro == "debian" ]]; then
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Downloading the apache2-prefork-dev package with apt-get..."
+	fi;
+	apt-get -y -q install apache2-prefork-dev;
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Compiling mod_zeus.c..."
+	fi;
+	apxs2 -iac mod_zeus.c;
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Adding module configuration to Apache."
+	fi;
+	echo "LoadModule zeus_module modules/mod_zeus.so" > /etc/apache2/mods-enabled/zeus.conf;
+	echo "ZeusEnable On" >> /etc/apache2/mods-enabled/zeus.conf;
+	echo "ZeusLoadBalancerIP *" >> /etc/apache2/mods-enabled/zeus.conf;
+#	echo "ZeusLoadBalancerIP 10.100.3.23 10.100.3.24" >> /etc/apache2/mods-enabled/zeus.conf;
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Restarting Apache..."
+	fi;
+	service apache2 graceful;
+
+	if [[ ${!#} != "-q" ]]; then
+		a8info "Done."
+	fi;
+else
+	a8fail "No supported distribution found"
+fi;
+
+rm mod_zeus.c;
